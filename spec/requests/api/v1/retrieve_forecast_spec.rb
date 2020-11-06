3e60506fbf54d05b8922d94b4429bbf7d06d5e58
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-05 20:14:48 -0700

    Creates forecast test

---
+++ spec/requests/api/v1/retrieve_forecast_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Retrieve weather' do
+  it 'retrieves current weather for a city' do
+    get '/api/v1/forecast?location=denver,co'
+
+    expect(response).to be_successful
+    forecast = JSON.parse(response.body, symbolize_names: true)
+
+  end
+end
+
+GET /api/v1/forecast?location=denver,co
+Content-Type: application/json
+Accept: application/json
