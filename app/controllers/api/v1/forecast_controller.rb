9d49db2acce1910a80b7df2303e517739321dd91
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-05 21:40:49 -0700

    Adds facades, services, and poros to app

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,14 +1,21 @@
 class Api::V1::ForecastController < ApplicationController
   def show
+    conn = Faraday.new('http://www.mapquestapi.com')
+    response = conn.get('/geocoding/v1/address') do |req|
+      req.params['key'] = ENV['MAP_API_KEY']
+      req.params['location'] = params[:location]
+    end
+
+    city_coordinates = JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
+
     conn = Faraday.new('https://api.openweathermap.org')
     response = conn.get('/data/2.5/onecall') do |req|
       req.params['appid'] = ENV['WEATHER_API_KEY']
-      req.params['lat'] = 39.7392
-      req.params['lon'] = -104.9903
+      req.params['lat'] = "#{city_coordinates[:lat]}"
+      req.params['lon'] = "#{city_coordinates[:lng]}"
       req.params['exclude'] = 'minutely,alerts'
     end
     data = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
   end
 end
 
