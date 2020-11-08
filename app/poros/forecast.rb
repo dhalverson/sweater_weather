9cba68d8811e64dfe401fb15ac2579f9152072f0
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-07 21:17:19 -0700

    Adds daily poro and helper

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,4 +1,7 @@
 class Forecast
+  attr_reader :current,
+              :hourly,
+              :daily
 
   def initialize(attr)
     @current = current_forecast(attr[:current])
@@ -15,6 +18,12 @@ class Forecast
   def hourly_forecast(hourly_data)
     hourly_data[0..7].map do |hour|
       HourlyForecast.new(hour)
+    end
+  end
+
+  def daily_forecast(daily_data)
+    daily_data[0..4].map do |day|
+      DailyForecast.new(day)
     end
   end
 end
