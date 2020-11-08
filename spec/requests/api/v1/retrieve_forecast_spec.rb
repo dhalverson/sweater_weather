4364e60e8329096cf651509483067f6802214c53
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-07 22:38:01 -0700

    Adds forecast request daily tests

--- spec/requests/api/v1/retrieve_forecast_spec.rb
+++ spec/requests/api/v1/retrieve_forecast_spec.rb
@@ -30,7 +30,13 @@ RSpec.describe 'Retrieve weather' do
     expect(forecast[:data][:attributes][:current_weather][:description]).to be_a(String)
     expect(forecast[:data][:attributes][:current_weather][:icon]).to be_a(String)
     
-    
+    expect(forecast[:data][:attributes][:daily_weather][:date]).to be_a(String)
+    expect(forecast[:data][:attributes][:daily_weather][:sunrise]).to be_a(String)
+    expect(forecast[:data][:attributes][:daily_weather][:sunset]).to be_a(String)
+    expect(forecast[:data][:attributes][:daily_weather][:max_temp]).to be_a(Float)
+    expect(forecast[:data][:attributes][:daily_weather][:min_temp]).to be_a(Float)
+    expect(forecast[:data][:attributes][:daily_weather][:conditions]).to be_a(String)
+    expect(forecast[:data][:attributes][:daily_weather][:icon]).to be_a(String)
   end
 end
 
