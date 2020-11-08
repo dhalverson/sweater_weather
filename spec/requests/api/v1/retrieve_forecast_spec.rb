d0e2732ce1c19528ffe73ff479af7edfe65787ef
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-07 22:30:34 -0700

    Update forecast request spec

--- spec/requests/api/v1/retrieve_forecast_spec.rb
+++ spec/requests/api/v1/retrieve_forecast_spec.rb
@@ -6,6 +6,22 @@ RSpec.describe 'Retrieve weather' do
 
     expect(response).to be_successful
     forecast = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(forecast[:data]).to have_key :id    
+    expect(forecast[:data]).to have_key :type    
+    expect(forecast[:data]).to have_key :attributes  
+
+    expect(forecast[:data][:attributes]).to have_key :current_weather
+    expect(forecast[:data][:attributes]).to have_key :daily_weather
+    expect(forecast[:data][:attributes]).to have_key :hourly_weather
+
+    expect(forecast[:data][:attributes][:current_weather]).to be_a(Hash)
+    expect(forecast[:data][:attributes][:daily_weather]).to be_an(Array) 
+    expect(forecast[:data][:attributes][:hourly_weather]).to be_an(Array)
+
+    expect
+    
+    
   end
 end
 
