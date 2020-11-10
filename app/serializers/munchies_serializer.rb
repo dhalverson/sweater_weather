73b0a0e3e7e335c0f414f77e61e0e564a5d13770
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-10 09:21:42 -0700

    Add serializer

---
+++ app/serializers/munchies_serializer.rb
@@ -0,0 +1,9 @@
+class MunchiesSerializer
+  include FastJsonapi::ObjectSerializer
+  set_id 'nil?'
+  set_type :munchie
+  attributes  :destination_city,
+              :travel_time,
+              :forecast,
+              :restaurant
+end
