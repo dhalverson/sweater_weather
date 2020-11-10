389ed903052d148962eb5ea3c884104b9918de50
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-10 15:48:49 -0700

    Update background image spec testing

--- spec/requests/api/v1/background_image_spec.rb
+++ spec/requests/api/v1/background_image_spec.rb
@@ -12,17 +12,19 @@ RSpec.describe 'Get background image for city' do
     expect(result[:data]).to have_key :id
     expect(result[:data]).to have_key :type
     expect(result[:data]).to have_key :attributes
-
     expect(result[:data][:type]).to be_a(String)
     expect(result[:data][:type]).to eq('image')
     expect(result[:data][:attributes]).to be_a(Hash)
-    expect(result[:data][:attributes]).to have_key :location
-    expect(result[:data][:attributes][:location]).to be_a(String)
-    expect(result[:data][:attributes]).to have_key :image_url
-    expect(result[:data][:attributes][:image_url]).to be_a(String)
-    expect(result[:data][:attributes]).to have_key :credit
-    expect(result[:data][:attributes][:credit]).to be_a(Hash)
-    expect(result[:data][:attributes][:credit][:description]).to be_a(String)
-    expect(result[:data][:attributes][:credit][:description]).to be_a(String)
+    expect(result[:data][:attributes]).to have_key :image
+    expect(result[:data][:attributes][:image]).to be_a(Hash)
+    expect(result[:data][:attributes][:image]).to have_key :location
+    expect(result[:data][:attributes][:image][:location]).to be_a(String)
+    expect(result[:data][:attributes][:image]).to have_key :image_url
+    expect(result[:data][:attributes][:image][:image_url]).to be_a(String)
+    expect(result[:data][:attributes][:image]).to have_key :credit
+    expect(result[:data][:attributes][:image][:credit]).to have_key :description
+    expect(result[:data][:attributes][:image][:credit][:description]).to be_a(String)
+    expect(result[:data][:attributes][:image][:credit]).to have_key :description
+    expect(result[:data][:attributes][:image][:credit][:credit]).to be_a(String)
   end
 end
