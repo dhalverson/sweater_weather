e090e3c2e81743b0431a99c2133ee4944808d8e2
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-10 17:33:41 -0700

    Update user password confirmation validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,14 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      render json: UsersSerializer.new(@user)
+      render json: UsersSerializer.new(@user), status: 201
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   private
-    def user_params
-      params.permit(:email, :password)
-    end
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
+  validates :password_confirmation, presence: true
 
   before_create do
     self.api_key = SecureRandom.hex
