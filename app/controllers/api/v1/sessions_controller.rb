46a716d8c8aae406298e581dade6e8c9b06759bb
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-10 19:02:01 -0700

    Adds login spec, route, and controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render json: UsersSerializer.new(user), status: 200
+    else
+      render json: 'Invalid Credentials', status: 401
+    end
+  end
+end
+
+
+
--- spec/requests/api/v1/login_spec.rb
+++ spec/requests/api/v1/login_spec.rb
@@ -2,20 +2,39 @@ require 'rails_helper'
 
 RSpec.describe 'User Login' do
   it 'allows a user to login' do
+    user = User.create!(
+      email: 'whatever@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+    
     body = {
       "email": "whatever@example.com",
       "password": "password"
     }
     
     post '/api/v1/sessions', params: body
-
+    
     expect(response).to be_successful
-    expect(response.status).to eq(201)
+    expect(response.status).to eq(200)
 
     login = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+
     expect(login).to be_a(Hash)
+    expect(login).to have_key :data
 
+    expect(login[:data]).to have_key :id
+    expect(login[:data]).to have_key :type
+    expect(login[:data]).to have_key :attributes
 
+    expect(login[:data][:id]).to be_a(String)
+    expect(login[:data][:type]).to be_a(String)
+    expect(login[:data][:type]).to eq('users')
+    expect(login[:data][:attributes]).to be_a(Hash)
+    expect(login[:data][:attributes]).to have_key :email
+    expect(login[:data][:attributes][:email]).to be_a(String)
+    expect(login[:data][:attributes][:email]).to eq('whatever@example.com')
+    expect(login[:data][:attributes]).to have_key :api_key
+    expect(login[:data][:attributes][:api_key]).to be_a(String)
   end
 end
