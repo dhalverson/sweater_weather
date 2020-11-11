e02f4fdcf5040c6cd083ee4c8c24fec64f6b3543
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-11-11 06:56:06 -0700

    Updates travel time method in road trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -9,25 +9,29 @@ class RoadTripFacade
         weather_at_eta: 'Weather unavailable'
       }
     else
-      travel_time_hours = (((RoadTripFacade.get_travel_time(origin, destination).to_f / 60) / 60).round + 1)
-
+      travel_time_hours_rounded = (((RoadTripFacade.get_travel_time(origin, destination).to_f / 60) / 60).round + 1)
+      travel_time_seconds = RoadTripFacade.get_travel_time(origin, destination)
       data = {
         start_city: origin,
         end_city: destination,
-        travel_time: Time.at(RoadTripFacade.get_travel_time(origin, destination)).utc.strftime("%H:%M"),
+        travel_time: RoadTripFacade.seconds_to_hours_minutes(travel_time_seconds),
         weather_at_eta: {
-          temperature: RoadTripFacade.get_destination_forecast(destination)[:hourly][travel_time_hours][:temp],
-          conditions: RoadTripFacade.get_destination_forecast(destination)[:hourly][travel_time_hours][:weather][0][:description]
+          temperature: RoadTripFacade.get_destination_forecast(destination)[:hourly][travel_time_hours_rounded][:temp],
+          conditions: RoadTripFacade.get_destination_forecast(destination)[:hourly][travel_time_hours_rounded][:weather][0][:description]
         }
       }
     end
     RoadTrip.new(data)
-    # Time does not work over 24 hours, so I would if I had more time update this
+    # time working, would refactor for less calls
   end
 
   private_class_method
 
-    def self.get_travel_time(origin, destination)
+  def self.seconds_to_hours_minutes(seconds)
+    '%02d:%02d' % [seconds / 3600, seconds / 60 % 60]
+  end
+
+  def self.get_travel_time(origin, destination)
     road_trip = LocationService.get_distance(origin, destination)
     road_trip[:route][:realTime] ||= 'Impossible Route'
   end
